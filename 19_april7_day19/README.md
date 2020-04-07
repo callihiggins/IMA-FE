d9d6222f3564baca3ccc8a41668900fc97f36c84
Calli <calli@nyu.edu>
2020-04-07 18:25:28 -0400

    adding day 19

---
+++ 19_april7_day19/README.md
@@ -0,0 +1,32 @@
+* Review
+  * OOP
+
+* New things
+  * Linking scripts to our HTML
+  * DOM
+    * [Nodes: Parent, Children, Siblings](https://github.com/thejsway/thejsway/blob/master/manuscript/chapter13.md#web-page-structure)
+    * NodeLists - `document.body.childNodes`
+    * Selecting classes, id, elements
+    * Seeing innerHtml, classList on a node
+  * Modifying the DOM
+    * Adjusting innerHtml, textContent on a node
+    * Add/Remove/Toggle classList
+    * createElement
+    * appendChild, replaceChild, insertBefore, insertAdjacent
+  * MadLibs
+
+  ## Homework
+  In [The Javascript Way](https://github.com/thejsway/thejsway)
+    * Review Chapters 13 - 16 (no need to turn in the assignments but you should attempt them anyway)
+    * Coding exercise:
+      * Begin by creating an HTML document that has at least 10 of the same HTML elements.
+      * Wrap some of these inside of `<div>` tags. Assign an ID to at least two of your `<div>` tags.
+      * Build three buttons that will run a function on click.
+      * Add three empty divs to the bottom of your page with unique IDs.
+      * Now, create at least three functions that will run on click:
+        * How many of the same HTML elements are on the page. (For example, if you included 10 `<p>`  tags, it should return a statement that says something like, "There are 10 paragraph tags on this page.")
+        * How many elements are inside of your first ID
+        * How many elements are inside of your second ID
+        * Each function should update one of the three empty divs with this information
+    * Submit a link to your GITHUB Repo for this code on NYU classes by the start of our next class Thursday, 4/9.
+
---
+++ 19_april7_day19/jsdom/script.js
@@ -0,0 +1,53 @@
+const heading = document.getElementById("title");
+
+const name = "Calli's";
+
+heading.innerText =`Enter ${name} Information`;
+
+document.getElementById("main").innerHTML = "<h3> Hello World Smaller</h3>";
+
+const paragraphs = document.getElementsByTagName("p");
+console.log(paragraphs[0].innerText);
+
+const inputs =  document.getElementsByClassName("inputItem");
+inputs[1].classList.add('pink');
+
+const newElement = document.createElement('div')
+newElement.innerHTML = `I'm an element created on the fly`;
+newElement.style.fontSize = '24px';
+document.querySelector('p').appendChild(newElement);
+
+//adding an event listener
+//addEventListner requires two parameters: the type of event, the event that is occuring (callback)
+//a callback is a function in a function
+heading.addEventListener("click", function () {
+  alert("you clicked me!");
+});
+
+function myFunction() {
+  //if input is equal to the actual letter, alert them they got it right
+  const letter = document.getElementById("myInput").value;
+  if (letter === "c") {
+    alert("You guessed correctly!");
+  } else {
+    alert("You guessed the wrong letter!");
+  }
+}
+
+document.getElementById('submitLetter').addEventListener('click', e => {
+  myFunction();
+})
+
+//include a few text boxes in HTML to grab a person's name, an adjective, and a noun
+//write a JS function that will concatenate all of the user input into a MAD LIB. Make it funny.
+// deliver the completed MAD LIB back to the user in an alert
+function madLib() {
+  const name = document.getElementById("myInputName").value;
+  const adjective = document.getElementById("myInputAdjective").value;
+  const noun = document.getElementById("myInputNoun").value;
+  document.getElementById("result").innerHTML = `Once upon a time, ${name} flew ${adjective} on a ${noun}`;
+}
+
+document.getElementById('runMadlib').addEventListener('click', e => {
+  madLib();
+})
