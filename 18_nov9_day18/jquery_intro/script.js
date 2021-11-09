8d43630ee2273bbafed67556daeb8983fc024372
callihiggins <calli@nyu.edu>
2021-11-09 13:49:06 -0500

    Update class 18

--- 18_nov9_day18/jquery_intro/script.js
+++ 18_nov9_day18/jquery_intro/script.js
@@ -3,17 +3,12 @@ const app = {
   data: detailedBandObject, //this comes from my data.js files
 
   initialize: () => {
-    $('.box').click(function () {
-      // When jQuery calls a handler, the this keyword is a reference to the element
-      // where the event is being delivered; for directly bound events this is the element
-      // where the event was attached and for delegated events this is an element matching selector.
-      // (Note that this may not be equal to event.target if the event has bubbled from a descendant element.)
-      // To create a jQuery object from the element so that it can be used with jQuery methods, use $( this ).
-      $(this).toggleClass('transform')  
+    $('.box').click(e => {
+      $(e.currentTarget).toggleClass('transform')  
     });
 
-    $('.circle').dblclick(function () {
-      $(this).toggleClass('moveMe')      
+    $('.circle').dblclick(e => {
+      $(e.currentTarget).toggleClass('moveMe')      
     });
 
     $('.listBeatles').click(function () {
@@ -33,12 +28,12 @@ const app = {
     });
   },
 
-  compareBands: function () {
+  compareBands: () => {
     const nirvanaMembers = app.data.nirvana.members;
     const beatlesMembers = app.data.beatles.members;
 
     $('.bandsComparison').text(
       `${nirvanaMembers.length > beatlesMembers.length ? 'Nirvana' : 'Beatles'} 
-    have more members`);
+    had more members`);
   }
 }
